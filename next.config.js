9a7d2aae3bade8742d602c16a2ab57adbf303cb5
Dmitry Neverovski <dmitryneverovski@gmail.com>
2023-02-21 12:53:28 +0300

    chore: init project

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  compress: false,
+  poweredByHeader: false,
+  reactStrictMode: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  }
+};
---
+++ src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const ENV_DEVELOPMENT = 'development';
+export const ENV_PRODUCTION = 'production';
